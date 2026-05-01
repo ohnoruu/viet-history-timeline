f5b94b89dab5dd2b0b69cc543ff823257b34eefb
RubyD <143292762+ohnoruu@users.noreply.github.com>
2026-05-01 19:15:19 +0000

    Added timeline items

--- viet-timeline/src/timeline.js
+++ viet-timeline/src/timeline.js
@@ -1,51 +1,72 @@
 export const timelineData = [
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/conrongchautien.png`,
-        year: "2879 BC",
-        title: "Truyền Thuyết Con Rồng Cháu Tiên",
-        description: "Legends tell of the dragon king Lạc Long Quân, who rose from the sea, and the fairy Âu Cơ, who descended from the highlands. Lạc Long Quân and Âu Cơ defended the land from evil spirits, and gave birth to 100 children. They eventually separated, with Lạc Long Quân taking 50 children to the sea and Âu Cơ taking 50 children to the mountains. The eldest son of Âu Cơ became the first Hùng King, marking the beginning of the Hùng Kings' rule over Văn Lang, the first Vietnamese kingdom. There were 18 Hùng Kings in total, lasting for 1000 years (2879 BC - 258 BC).",
-        descriptionViet: "Truyền thuyết kể về Lạc Long Quân, con rồng từ biển, và Âu Cơ, con tiên từ núi. Hai người cùng bảo vệ đất nước khỏi điều xấu và sinh ra 100 người con. Lạc Long Quân đưa 50 con xuống biển, còn Âu Cơ đưa 50 con lên núi. Người con cả của Âu Cơ trở thành vua Hùng đầu tiên. Có 18 vua Hùng, kéo dài khoảng 1000 năm (2879 BC - 258 BC)."
-    },
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/haibatrung.png`,
-        year: "14-43",
-        title: "Hai Bà Trưng",
-        description: "Vietnam faced conflict with China for 1000 years. In 40 AD, the Trung sisters, Trưng Trắc and Trưng Nhị, led a rebellion against the Chinese Han dynasty. They successfully drove out the Chinese and ruled for three years before being defeated by the Han general, jumping into the Hát river to avoid capture.",
-        descriptionViet: "Việt Nam đã có xung đột với Trung Quốc trong 1000 năm. Vào năm 40, hai chị em Trưng Trắc và Trưng Nhị lãnh đạo một cuộc nổi dậy chống lại nhà Hán. Họ đã đuổi được quân Trung Quốc và cai trị trong ba năm. Sau đó, họ bị tướng Hán đánh bại và nhảy xuống sông Hát để không bị bắt."
-    },
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/ngoquyen.png`,
-        year: "899-979",
-        title: "Ngô Quyền và Đinh Bộ Lĩnh",
-        description: "After centuries of Chinese domination, Ngô Quyền declared independence in 939 AD. He famously defeated the Southern Han navy at the Battle of Bạch Đằng River by strategically placing iron-tipped wooden stakes in the riverbed, which impaled the enemy ships during low tide. In the year 944 AD, Ngô Quyền died, leading to political instability and a period of civil war known as the Anarchy of the 12 Warlords, which lasted until 968 AD when Đinh Bộ Lĩnh, the first emperor, unified the country. Đinh Bộ Linh was a triumphant warlord and defeated the other 11 warlords to unify the country.",
-        descriptionViet: "Sau nhiều năm bị Trung Quốc cai trị, Ngô Quyền giành độc lập vào năm 939. Ông thắng trận trên sông Bạch Đằng bằng cách đặt cọc gỗ nhọn dưới nước để chặn tàu địch khi nước xuống. Năm 944, Ngô Quyền qua đời, đất nước không ổn định và có nhiều nhóm đánh nhau. Thời gian này gọi là thới kỹ loạn 12 sứ quân. Đến năm 968, Đinh Bộ Lĩnh thống nhất đất nước và trở thành vua đầu tiên. Ông đánh bại 11 sứ quân khác để thống nhất Việt Nam."},
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/lythoungkiet.png`,
-        year: "1019-1300",
-        title: "Cuộc chiến chống lại Trung Quốc vẫn tiếp diễn",
-        description: "Vietnam continued facing invasions from China. Lý Thương Kiết, a beloved general, led forces to victory against the Song dynasty in 1075 AD at the Battle of Như Nguyệt River. The Trần dynasty successfully repelled three Mongol invasions in the 13th century, with General Trần Hưng Đạo famously defeating the Mongols at the Battle of Bạch Đằng River in 1288 AD using a similar strategy to Ngô Quyền. Other notable generals include Lê Lợi, who led the Lam Sơn uprising against the Ming dynasty in the 15th century, and Nguyễn Huệ, who defeated the Qing dynasty in the late 18th century.",
-       descriptionViet: "Việt Nam tiếp tục phòng thủ chống lại Trung Quốc. Năm 1075, tướng Lý Thường Kiệt đã đánh bại quân nhà Tống ở sông Như Nguyệt. Vào thế kỷ 13, nhà Trần đã ba lần đánh thắng quân Mông Cổ. Tướng Trần Hưng Đạo nổi tiếng với chiến thắng trên sông Bạch Đằng năm 1288, dùng cách giống Ngô Quyền. Sau đó, Lê Lợi lãnh đạo khởi nghĩa Lam Sơn chống nhà Minh vào thế kỷ 15, và Nguyễn Huệ đánh bại quân Thanh vào cuối thế kỷ 18."
-    },
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/frenchcolonization.png`,
-        year: "1858-1954",
-        title: "Sự thuộc dịa hóa của Pháp và WWII",
-        description: "Vietnam was colonized by the French in the mid-19th century, leading to significant cultural and political changes. The Vietnamese people resisted French rule through various uprisings and movements, such as the Cần Vương movement in the late 19th century. During World War II, Japan occupied Vietnam, further complicating the struggle for independence. After the war, the Viet Minh, led by Ho Chi Minh, declared independence in 1945, but this was not recognized by the French, leading to the First Indochina War (1946-1954). The war ended with the Geneva Accords, which temporarily divided Vietnam at the 17th parallel.",
-        descriptionViet: "Vào giữa thế kỷ 19, Việt Nam bị Pháp đô hộ. Người Việt đã chống lại Pháp qua nhiều, như phong trào Cần Vương vào cuối thế kỷ 19. Trong WWII, Nhật Bản chiếm Việt Nam, làm tình hình thêm khó khăn. Sau chiến tranh, Hồ Chí Minh lãnh đạo tuyên bố độc lập năm 1945, nhưng Pháp không dồng ý. Chiến tranh Indochina bắt dầu vào năm 1946-1954. Sau chiến tranh, Geneva Accord chia rẻ Việt Năm ở vĩ tuyến 17."
-    },
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/vietnamwar.png`,
-        year: "1954-1975",
-        title: "Chiến tranh Việt Nam",
-        description: "French colonization led to significant political instability, leading to the divide between the communist North and the anti-communist South. The Vietnam War was a prolonged conflict that lasted from 1954 to 1975, involving North Vietnam and the Viet Cong against South Vietnam and the United States. The war ended with the Fall of Saigon in 1975, leading to the reunification of Vietnam under communist rule.",
-        descriptionViet: "Sau khi Pháp rời đi, Việt Nam bị chia thành hai miền: miền Bắc và miền Nam. Chiến tranh Việt Nam kéo dài từ 1954 đến 1975 giữa hai miền này, với sự tham gia của Hoa Kỳ. Cuộc chiến kết thúc khi Sài Gòn sụp đổ năm 1975, và Việt Nam được thống nhất thành một nước."
-    },
-    {
-        img: `${import.meta.env.BASE_URL}assets/timeline/migration.png`,
-        year: "1975-Present",
-        title: "Các làn sóng di cư và người Viết ở nước Mỹ",
-        description: "After the reunification of Vietnam, individuals suspected of aligning with the fallen Southern government were persecuted and sent to re-education camps. Conditions in the camps were very poor and often guaranteed death for those who were sent there. The following conditions, as well as shifting political changes, inspired waves of migration to other countries, with the wealthiest moving in the first waves and the general working class in later waves. Vietnamese people fled across different countries, including the Philippines, Thailand, and Germany. The majority of Vietnamese refugees settled in the United States, with the largest community residing in Orange County, California.",
-        descriptionViet: "Sau khi Việt Nam thống nhất, nhiều người bị nghi là ủng hộ chính quyền miền Nam bị bắt và đưa vào trại cải tạo. Điều kiện trong các trại rất khó khăn và nguy hiểm. Vì vậy, nhiều người Việt đã rời khỏi đất nước. Người giàu đi trước, còn người lao động đi sau. Người Việt đã đến nhiều nước như Phi Líp Pin, Thái Lan và Đức. Hơn nhứt người Việt Nam đến nước Mỹ, với cộng đồng lớn nhất thành lập ở Orange County, California."
-    },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/conrongchautien.png`,
+       year: "2879 BC",
+       title: "Truyền Thuyết Con Rồng Cháu Tiên",
+       description: "Legends tell of the dragon king Lạc Long Quân, who rose from the sea, and the fairy Âu Cơ, who descended from the highlands. Lạc Long Quân and Âu Cơ defended the land from evil spirits, and gave birth to 100 children. They eventually separated, with Lạc Long Quân taking 50 children to the sea and Âu Cơ taking 50 children to the mountains. The eldest son of Âu Cơ became the first Hùng King, marking the beginning of the Hùng Kings' rule over Văn Lang, the first Vietnamese kingdom. There were 18 Hùng Kings in total, lasting for 1000 years (2879 BC - 258 BC).",
+       descriptionViet: "Truyền thuyết kể về Lạc Long Quân, con rồng từ biển, và Âu Cơ, con tiên từ núi. Hai người cùng bảo vệ đất nước khỏi điều xấu và sinh ra 100 người con. Lạc Long Quân đưa 50 con xuống biển, còn Âu Cơ đưa 50 con lên núi. Người con cả của Âu Cơ trở thành vua Hùng đầu tiên. Có 18 vua Hùng, kéo dài khoảng 1000 năm (2879 BC - 258 BC)."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/haibatrung.png`,
+       year: "14-43",
+       title: "Hai Bà Trưng",
+       description: "Vietnam faced conflict with China for 1000 years. In 40 AD, the Trung sisters, Trưng Trắc and Trưng Nhị, led a rebellion against the Chinese Han dynasty. They successfully drove out the Chinese and ruled for three years before being defeated by the Han general, jumping into the Hát river to avoid capture.",
+       descriptionViet: "Việt Nam đã có xung đột với Trung Quốc trong 1000 năm. Vào năm 40, hai chị em Trưng Trắc và Trưng Nhị lãnh đạo một cuộc nổi dậy chống lại nhà Hán. Họ đã đuổi được quân Trung Quốc và cai trị trong ba năm. Sau đó, họ bị tướng Hán đánh bại và nhảy xuống sông Hát để không bị bắt."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/ngoquyen.png`,
+       year: "899-979",
+       title: "Ngô Quyền và Đinh Bộ Lĩnh",
+       description: "After centuries of Chinese domination, Ngô Quyền declared independence in 939 AD. He famously defeated the Southern Han navy at the Battle of Bạch Đằng River by strategically placing iron-tipped wooden stakes in the riverbed, which impaled the enemy ships during low tide. In the year 944 AD, Ngô Quyền died, leading to political instability and a period of civil war known as the Anarchy of the 12 Warlords, which lasted until 968 AD when Đinh Bộ Lĩnh, the first emperor, unified the country. Đinh Bộ Linh was a triumphant warlord and defeated the other 11 warlords to unify the country.",
+       descriptionViet: "Sau nhiều năm bị Trung Quốc cai trị, Ngô Quyền giành độc lập vào năm 939. Ông thắng trận trên sông Bạch Đằng bằng cách đặt cọc gỗ nhọn dưới nước để chặn tàu địch khi nước xuống. Năm 944, Ngô Quyền qua đời và có nhiều nhóm đánh nhau. Thời gian này gọi là thời kỳ loạn 12 sứ quân. Đến năm 968, Đinh Bộ Lĩnh thống nhất đất nước và trở thành vua đầu tiên. Ông đánh bại 11 sứ quân khác để thống nhất Việt Nam."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/lythoungkiet.png`,
+       year: "1019-1300",
+       title: "Lý Thương Kiết và Trần Hưng Đạo",
+       description: "Lý Thương Kiết was a beloved general that fought back against the Song dynasty, killing 8000 Chinese troops in 3 days. He wrote a 4 verse poem that was considered Vietnam’s first declaration of independence. The famous general Trần Hưng Đạo fought against the Mongolians, who caused conflicts across Europe and Asia. He used Ngô Quyên’s legendary strategy to trap Mongolian ships during low tide.",
+       descriptionViet: "Tương giỏi Lý Thương Kiết đánh nhau với nhà Song, giết chết 8000 quân Trung Quốc vào 3 ngày. Vào năm 1287, Mông Cổ nổi tiếng ở châu Âu và châu Á. Như Ngô Quyền, danh tướng Trần Hưng Đạo dặt cọc gỗ nhọn dươi nước để trặn tàu khi nước xưóng."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/leloi.png`,
+       year: "1385-1443",
+       title: "Lê Lợi và Lê Lai",
+       description: "Lê Lợi was the leader of a rebellion against the Minh dynasty and fought against them in a resistance war and becoming victorious after 10 years. It was told that he was lent a sword from a divine turtle to aid him in battle. In a near encounter with Minh soldiers who threatened to capture him (1419), general Lê Lai disguised himself as Lê Lợi to lead the soldiers away. Because of this sacrifice, Lê Lợi was able to live and defeat the Minh dynasty.",
+       descriptionViet: "Lê Lợi là người lãnh đạo đã khởi nghĩa chống lại quân Minh, đánh thắng quân Minh sau 10 năm. Truyền thuyết nói là Lê Lợi đánh nhà Minh với một thanh kiếm quý của con rùa. Vào năm 1419, tương Lê Lai cưứ Lê Lợi. Khi Lê Lợi sắp bị bắt, Lê Lai mặc quần áo của Lê Lợi để bị bắt. Vì vậy, Lê Lợi được sống và đánh thắng nhà Minh."
+   },
+{
+       img: `${import.meta.env.BASE_URL}assets/timeline/nguyenhue.png`,
+       year: "1753-1792",
+       title: "Vua Quang Trung - Nguyễn Huệ",
+       description: "Nguyễn Huệ was renowned for being a military genius. After 22 years of fighting both the North and South, he was able to defeat both Xiêm (Thailand) and Thanh (Chinese) dynasties, establishing solid independence for the country.",
+        descriptionViet: "Nguyễn Huệ là một thiên tài quân sự. Sau 22 năm đánh Nam dẹp Bắc, Nguyễn Huệ đánh tháng nhà Xiêm (Thái Lan) và nhà Thanh (Trung Quốc), thành lập sự độc lập của Việt Nam."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/frenchcolonization.png`,
+       year: "1858-1954",
+       title: "Sự thuộc dịa hóa của Pháp và WWII",
+       description: "Vietnam was colonized by the French in the mid-19th century, leading to significant cultural and political changes. The Vietnamese people resisted French rule through various uprisings and movements, such as the Cần Vương movement in the late 19th century. During World War II, Japan occupied Vietnam, further complicating the struggle for independence. After the war, the Viet Minh, led by Ho Chi Minh, declared independence in 1945, but this was not recognized by the French, leading to the First Indochina War (1946-1954). The war ended with the Geneva Accords, which temporarily divided Vietnam at the 17th parallel.",
+       descriptionViet: "Vào giữa thế kỷ 19, Việt Nam bị Pháp đô hộ. Người Việt đã chống lại Pháp qua nhiều, như phong trào Cần Vương vào cuối thế kỷ 19. Trong WWII, Nhật Bản chiếm Việt Nam, làm tình hình thêm khó khăn. Sau chiến tranh, Hồ Chí Minh lãnh đạo tuyên bố độc lập năm 1945, nhưng Pháp không dồng ý. Chiến tranh Indochina bắt dầu vào năm 1946-1954. Sau chiến tranh, Geneva Accord chia rẻ Việt Năm ở vĩ tuyến 17."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/vietnamwar.png`,
+       year: "1954-1975",
+       title: "Chiến tranh Việt Nam",
+       description: "French colonization led to significant political instability, leading to the divide between the communist North and the anti-communist South. The Vietnam War was a prolonged conflict that lasted from 1954 to 1975, involving North Vietnam and the Viet Cong against South Vietnam and the United States. The war ended with the Fall of Saigon in 1975, leading to the reunification of Vietnam under communist rule.",
+       descriptionViet: "Sau khi Pháp rời đi, Việt Nam bị chia thành hai miền: miền Bắc và miền Nam. Chiến tranh Việt Nam kéo dài từ 1954 đến 1975 giữa hai miền này, với sự tham gia của Hoa Kỳ. Cuộc chiến kết thúc khi Sài Gòn sụp đổ năm 1975, và Việt Nam được thống nhất thành một nước."
+   },
+   {
+       img: `${import.meta.env.BASE_URL}assets/timeline/migration.png`,
+       year: "1975-Present",
+       title: "Các làn sóng di cư và người Viết ở nước Mỹ",
+       description: "After the reunification of Vietnam, individuals suspected of aligning with the fallen Southern government were persecuted and sent to re-education camps. Conditions in the camps were very poor and often guaranteed death for those who were sent there. The following conditions, as well as shifting political changes, inspired waves of migration to other countries, with the wealthiest moving in the first waves and the general working class in later waves. Vietnamese people fled across different countries, including the Philippines, Thailand, and Germany. The majority of Vietnamese refugees settled in the United States, with the largest community residing in Orange County, California.",
+       descriptionViet: "Sau khi Việt Nam thống nhất, nhiều người bị nghi là ủng hộ chính quyền miền Nam bị bắt và đưa vào trại cải tạo. Điều kiện trong các trại rất khó khăn và nguy hiểm. Vì vậy, nhiều người Việt đã rời khỏi đất nước. Người giàu đi trước, còn người lao động đi sau. Người Việt đã đến nhiều nước như Phi Líp Pin, Thái Lan và Đức. Hơn nhứt người Việt Nam đến nước Mỹ, với cộng đồng lớn nhất thành lập ở Orange County, California."
+   },
 ]
+
+
+
+
+
+
 
